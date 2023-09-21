e0e13b1059f807d03d9e42bdbc129de926cd833c
zlatin-r <zlatinrusev@gmail.com>
2023-09-21 19:50:27 +0200

    Solution

--- Functions/Functions - Exercise/password_validator.py
+++ Functions/Functions - Exercise/password_validator.py
@@ -1,25 +1,21 @@
 password = input()
 
 
-def pass_check(password):
+def pass_check():
     flag = True
 
     if not (6 <= len(password) <= 10):
         flag = False
         print("Password must be between 6 and 10 characters")
-
     if not password.isalnum():
         flag = False
         print("Password must consist only of letters and digits")
-
     if sum(1 for char in password if char.isdigit()) < 2:
         flag = False
         print("Password must have at least 2 digits")
-
     if flag:
         print("Password is valid")
-
     exit()
 
 
-print(pass_check(password))
+print(pass_check())
