19399790e70092fb94b75c7f60ea768c3b47165e
zlatin-r <zlatinrusev@gmail.com>
2023-09-23 20:46:53 +0200

    Solution to the task

--- Lists Advance/Lists Advanced - Exercise/anonymous_threat.py
+++ Lists Advance/Lists Advanced - Exercise/anonymous_threat.py
@@ -7,7 +7,20 @@ if command[0] == "merge":
     end_index = int(command[2])
 
     for word in range(len(input_line)):
-        if word == start_index or word <= end_index:
-            result += input_line[word]
+        if end_index > len(input_line):
+            end_index = len(input_line)
         else:
-            result += ", " + input_line[word]
+            if word == start_index or word <= end_index:
+                result += input_line[word]
+            else:
+                result += ", " + input_line[word]
+
+if command[0] == "divide":
+    index = int(command[1])
+    partitions = int(command[2])
+
+    word = input_line[index]
+    list_of_letters = [x for x in word]
+    if len(list_of_letters) % 2 == 0:
+
+print(result)
