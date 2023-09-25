13e65f2ab7690f2ccc55f9e9cfab02ded2c6b037
zlatin-r <zlatinrusev@gmail.com>
2023-09-25 19:54:50 +0200

    Solution to the task

--- Lists Advance/Lists Advanced - Exercise/pokemon_dont_go.py
+++ Lists Advance/Lists Advanced - Exercise/pokemon_dont_go.py
@@ -1,35 +1,48 @@
-line_input = input().split(" ")
-sequence = [int(x) for x in line_input]
-removed_elements = 0
+sequence_int = [int(x) for x in input().split(" ")]
 
-while len(sequence) > 0:
-    index = int(input())
-    count = 0
 
-    if index < 0:
+def check_index(index_value, sequence):                     # problem
+    if index_value < 0:
         sequence.pop(0)
-        sequence.insert(0, sequence[-1])
-        removed_elements += sequence[index - 1]
-        continue
-    if index >= len(sequence):
+        sequence.append(sequence[-1])
+    elif index_value >= len(sequence):
         sequence.pop(-1)
         sequence.append(sequence[0])
-        removed_elements += sequence[index-1]
-        index -= 1
+    return index_value, sequence
 
-    removed_elements += sequence[index]
-    element = sequence[index]
 
-    for num in sequence:
-        if num <= element:
-            num += element
-            sequence[count] = num
-            count += 1
-        elif num >= element:
-            num -= element
-            sequence[count] = num
-            count += 1
+def remove_element(sequence, element):                     # works good
+    sequence.pop(element)
+    return sequence
 
-    sequence.pop(index)
 
-print(sequence)
+def increase_decrease(sequence):                            # works good
+    counter = 0
+    for number in sequence:
+        if number <= element_to_remove:
+            number += element_to_remove
+            sequence[counter] = number
+            counter += 1
+        else:
+            number -= element_to_remove
+            sequence[counter] = number
+            counter += 1
+    return sequence
+
+
+def sum_removed(element):                             # works good
+    removed_sum = 0
+    removed_sum += element
+    return removed_sum
+
+
+while len(sequence_int) != 0:
+    index = int(input())
+
+    check_index(index, sequence_int)
+
+    element_to_remove = int(sequence_int[index])    # problem
+
+    sum_removed(element_to_remove)
+    remove_element(sequence_int, index)
+    increase_decrease(sequence_int)
