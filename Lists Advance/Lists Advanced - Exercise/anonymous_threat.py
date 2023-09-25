8c0090ddbcf6a01ecd9801f3a745c220980c3379
zlatin-r <zlatinrusev@gmail.com>
2023-09-25 16:20:00 +0200

    Solution to the task

--- Lists Advance/Lists Advanced - Exercise/anonymous_threat.py
+++ Lists Advance/Lists Advanced - Exercise/anonymous_threat.py
@@ -1,5 +1,4 @@
 input_line = input().split(" ")
-command = input().split(" ")
 
 
 def merge(array):
@@ -8,30 +7,46 @@ def merge(array):
     end_index = int(command[2])
 
     for word in range(len(array)):
+        if start_index >= len(array):
+            merged_list = " ".join(array)
+            break
         if end_index > len(array):
             end_index = len(array)
+
+        if start_index <= word <= end_index:
+            merged_list += "".join(array[word])
+        elif word < start_index:
+            merged_list += "".join(array[word]) + " "
         else:
-            if word == start_index or word <= end_index:
-                merged_list += "".join(array[word])
-            else:
-                merged_list += " " + "".join(array[word])
-    array = merged_list
-    return array
+            merged_list += " " + "".join(array[word])
+    return merged_list.split(" ")
 
 
-print(merge(input_line))
-print(input_line)
-
-
-if command[0] == "divide":
+def divide(array):
     index = int(command[1])
     partitions = int(command[2])
 
-    word = input_line[index]
-    list_of_letters = [x for x in word]
+    string = input_line[index]
+    partition_length = len(string) // partitions
+    reminder = len(string) % partitions
 
-    if len(list_of_letters) % 2 == 0:
-        for i in range(0, len(list_of_letters), partitions):
-            result += ", ".join(list_of_letters[i:partitions])
+    new_parts = [string[i: i + partition_length] for i in
+                 range(0, len(string), partition_length)]
+    if reminder > 0:
+        new_parts[-1] += string[-reminder:]
+    input_line[index:index + 1] = new_parts
+    return input_line
 
-print(result)
+
+while True:
+    command = input().split(" ")
+    if command[0] == "merge":
+        input_line = merge(input_line)
+        print(input_line)
+
+    elif command[0] == "divide":
+        input_line = divide(input_line)
+        print(divide(input_line))
+    elif command[0] == "3:1":
+        print(input_line)
+        break
