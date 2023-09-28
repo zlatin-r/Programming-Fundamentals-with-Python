48fc95b7a5bb856d58a8b77198f36278343ea936
zlatin-r <zlatinrusev@gmail.com>
2023-09-28 16:08:33 +0200

    Solution to the task (with help from ChatGPT)

--- Lists Advance/Lists Advanced - Exercise/softuni_course_planning.py
+++ Lists Advance/Lists Advanced - Exercise/softuni_course_planning.py
@@ -2,9 +2,12 @@ schedule = input().split(", ")
 
 while True:
     command = input().split(":")
+
+    if command[0] == "course start":
+        break
+
     action = command[0]
     lesson_title = command[1]
-    result = []
 
     if action == "Add" and lesson_title not in schedule:
         schedule.append(lesson_title)
@@ -15,19 +18,35 @@ while True:
     elif action == "Remove":
         if lesson_title in schedule:
             schedule.remove(lesson_title)
-
     elif action == "Swap":
-        lesson_title_2 = command[2]
-        if lesson_title in schedule and lesson_title_2 in schedule:
-            index_1 = schedule.index(lesson_title)
-            index_2 = schedule.index(lesson_title_2)
-            schedule[index_1], schedule[index_2] = schedule[index_2], schedule[index_1]
-
+        lesson_title2 = command[2]
+        if lesson_title in schedule and lesson_title2 in schedule:
+            index1 = schedule.index(lesson_title)
+            index2 = schedule.index(lesson_title2)
+            schedule[index1], schedule[index2] = schedule[index2], schedule[index1]
+            if f"{lesson_title}-Exercise" in schedule and f"{lesson_title2}-Exercise" in schedule:
+                index1_exercise = schedule.index(f"{lesson_title}-Exercise")
+                index2_exercise = schedule.index(f"{lesson_title2}-Exercise")
+                schedule[index1_exercise], schedule[index2_exercise] = schedule[index2_exercise], schedule[
+                    index1_exercise]
+            elif f"{lesson_title}-Exercise" in schedule:
+                index1_exercise = schedule.index(f"{lesson_title}-Exercise")
+                schedule.remove(f"{lesson_title}-Exercise")
+                schedule.insert(index2 + 1, f"{lesson_title}-Exercise")
+            elif f"{lesson_title2}-Exercise" in schedule:
+                index2_exercise = schedule.index(f"{lesson_title2}-Exercise")
+                schedule.remove(f"{lesson_title2}-Exercise")
+                schedule.insert(index1 + 1, f"{lesson_title2}-Exercise")
     elif action == "Exercise":
-        if lesson_title not in schedule:
-            schedule.append(lesson_title)
-            schedule.append(lesson_title + "-Exercise")
-        else:
+        if lesson_title in schedule:
             index = schedule.index(lesson_title)
-            schedule[index] = lesson_title + "-Exercise"
-    print(schedule)
+            if index + 1 < len(schedule) and not schedule[index + 1].endswith('-Exercise'):
+                schedule.insert(index + 1, f"{lesson_title}-Exercise")
+            elif index + 1 == len(schedule):
+                schedule.append(f"{lesson_title}-Exercise")
+        else:
+            schedule.append(lesson_title)
+            schedule.append(f"{lesson_title}-Exercise")
+
+for i, lesson in enumerate(schedule, start=1):
+    print(f"{i}.{lesson}")
