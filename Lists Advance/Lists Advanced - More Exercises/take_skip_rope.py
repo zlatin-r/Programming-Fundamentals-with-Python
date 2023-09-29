5eca32f01c4fadd77d8bca3b6c99a618f1e8c5de
zlatin-r <zlatinrusev@gmail.com>
2023-09-29 13:47:56 +0200

    Solution to the task

--- Lists Advance/Lists Advanced - More Exercises/take_skip_rope.py
+++ Lists Advance/Lists Advanced - More Exercises/take_skip_rope.py
@@ -7,7 +7,23 @@ non_numbers_list = [x for x in initial_string if not x.isdigit()]
 take_list = [number for index, number in enumerate(number_list) if index % 2 == 0]
 skip_list = [number for index, number in enumerate(number_list) if index % 2 != 0]
 
+count = len(take_list)
+take = [count, ]
 
 
+while count > 0:
+    for m in take_list:
+        taken_string = non_numbers_list[0:m]
+        result += "".join(taken_string)
+        non_numbers_list = non_numbers_list[m:]
+        take_list.remove(m)
+        break
 
+    for n in skip_list:
+        skipped_string = non_numbers_list[0:n]
+        non_numbers_list = non_numbers_list[n:]
+        skip_list.remove(n)
+        break
+    count -= 1
 
+print(result)
