6b99547f27d1e8591944e6456a51c337dc800a5e
zlatin-r <zlatinrusev@gmail.com>
2023-09-29 12:26:02 +0200

    Solution to the task

--- Lists Advance/Lists Advanced - More Exercises/social_distribution.py
+++ Lists Advance/Lists Advanced - More Exercises/social_distribution.py
@@ -4,12 +4,10 @@ counter = 0
 result = ""
 
 for number in population:
-
     if number < minimum_wealth:
         wealth_needed = minimum_wealth - number
         max_wealth = max(population)
         index_max_wealth = population.index(max_wealth)
-
         if max_wealth - wealth_needed < minimum_wealth:
             result = "No equal distribution possible"
             break
