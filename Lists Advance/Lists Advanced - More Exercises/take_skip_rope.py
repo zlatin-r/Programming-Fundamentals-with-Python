33d3a4bd8351a827af475b26cf579029f43a3388
zlatin-r <zlatinrusev@gmail.com>
2023-09-29 12:47:30 +0200

    Solution to the task

--- Lists Advance/Lists Advanced - More Exercises/take_skip_rope.py
+++ Lists Advance/Lists Advanced - More Exercises/take_skip_rope.py
@@ -1,11 +1,13 @@
-initial_string = [x for x in input().split(" ")]
+initial_string = list(input())
 result = ""
 
 number_list = [int(x) for x in initial_string if x.isdigit()]
 non_numbers_list = [x for x in initial_string if x.isalpha()]
 
-take_list = [x for x in number_list if number_list.index(x) % 2 == 0]
-skip_list = [x for x in number_list if number_list.index(x) % 2 != 0]
+take_list = [number for index, number in enumerate(number_list) if index % 2 == 0]
+skip_list = [number for index, number in enumerate(number_list) if index % 2 != 0]
+
+
 
 
 
