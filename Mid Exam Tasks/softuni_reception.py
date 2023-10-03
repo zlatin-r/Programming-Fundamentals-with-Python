c344ca78956bee850fb237015b2e80c79c6a8a1a
zlatin-r <zlatinrusev@gmail.com>
2023-10-03 14:07:30 +0200

    Task

--- Mid Exam Tasks/softuni_reception.py
+++ Mid Exam Tasks/softuni_reception.py
@@ -1,16 +1,14 @@
-from math import ceil
-
 first_employee_efficiency = int(input())
 second_employee_efficiency = int(input())
 third_employee_efficiency = int(input())
 total_students = int(input())
+total_time_needed = 0
 
 total_employees_efficiency = first_employee_efficiency + second_employee_efficiency + third_employee_efficiency
 
-total_time_needed = ceil(total_students / total_employees_efficiency)
-
-if total_time_needed > 3:
-    extra_time = total_time_needed // 3
-    total_time_needed += extra_time
+while total_students > 0:
+    total_time_needed += 1
+    if total_time_needed % 4 != 0:
+        total_students -= total_employees_efficiency
 
 print(f"Time needed: {total_time_needed}h.")
