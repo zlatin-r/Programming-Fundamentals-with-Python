12ca6afdf454b2ec1896435bbe475b09a68f7d06
zlatin-r <zlatinrusev@gmail.com>
2023-10-03 15:52:31 +0200

    Task

--- Mid Exam Tasks/array_modifier.py
+++ Mid Exam Tasks/array_modifier.py
@@ -1,2 +1,19 @@
 initial_list = [int(x) for x in input().split(' ')]
+command = input().split(' ')
 
+while command[0] != "end":
+
+    if command == "swap":
+        index_1 = int(command[1])
+        index_2 = int(command[2])
+
+        initial_list[index_1], initial_list[index_2] = initial_list[index_2], initial_list[index_1]
+
+    elif command == "multiply":
+        index_1 = int(command[1])
+        index_2 = int(command[2])
+
+        product = initial_list[index_1] * initial_list[index_2]
+        initial_list.insert(0, product)
+
+    elif
