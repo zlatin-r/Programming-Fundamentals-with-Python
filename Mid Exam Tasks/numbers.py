5b4f5f48e733991f8808023e0e0d224b6d8cde03
zlatin-r <zlatinrusev@gmail.com>
2023-10-03 16:14:35 +0200

    Task

--- Mid Exam Tasks/numbers.py
+++ Mid Exam Tasks/numbers.py
@@ -1,3 +1,17 @@
 initial_list = [int(x) for x in input().split(" ")]
+greater_list = []
+
+average_number = sum(initial_list) / len(initial_list)
+
+for num in initial_list:
+    if num > average_number:
+        greater_list.append(num)
+
+greater_list.sort(reverse=True)
+
+if len(greater_list) > 0:
+    print(", ".join(str(x) for x in greater_list))
+else:
+    print("No")
 
 
