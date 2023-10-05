88a4d7ccfa0a362b8d1552739c485f6a96956e5b
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2023-10-05 16:38:32 +0200

    Update moving_target.py
    final solution

--- Mid Exam Tasks/moving_target.py
+++ Mid Exam Tasks/moving_target.py
@@ -1,47 +1,41 @@
 targets = [int(x) for x in input().split(" ")]
 result_targets = []
-result = ""
 
 while True:
     command = input().split(" ")
     action = command[0]
-    
+
     if action == "End":
         break
+
     elif action == "Shoot":
         index = int(command[1])
         power = int(command[2])
-        
         if 0 <= index <= len(targets) - 1:
             targets[index] -= power
             if targets[index] <= 0:
                 targets.pop(index)
-        else:
-            result = "Invalid placement!"
-        
+
     elif action == "Add":
         index = int(command[1])
-        val = int(command[2])
-        
+        value = int(command[2])
         if 0 <= index <= len(targets) - 1:
-            targets.insert(index, val)
+            targets.insert(index, value)
         else:
-            result = "Invalid placement!"
-        
+            print("Invalid placement!")
+
     elif action == "Strike":
         index = int(command[1])
         radius = int(command[2])
-        
-        if index-radius >= 0 and index+radius <= len(targets) -1:
+        if index - radius >= 0 and index + radius <= len(targets) - 1:
             for i in range(len(targets)):
-                if index-radius <= i <= index+radius:
-                    continue
+                if index - radius <= i <= index + radius:
+                    pass
                 else:
                     result_targets.append(targets[i])
             targets = result_targets
         else:
-            result = "Strike missed!"
-            continue            
-            
-print(result)
+            print("Strike missed!")
+            continue
+
 print("|".join(str(x) for x in targets))
