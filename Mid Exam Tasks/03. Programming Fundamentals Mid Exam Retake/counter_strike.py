5d123300a590cf9dfcca7d136fa9b5bb9da3c37c
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2023-10-05 17:16:35 +0200

    Initial commit

---
+++ Mid Exam Tasks/03. Programming Fundamentals Mid Exam Retake/counter_strike.py
@@ -0,0 +1,17 @@
+energy = int(input())
+won_battles = 0
+
+while True:
+    distance = input()
+    if distance == "End of battle":
+        print(f"Won battles: {won_battles}. Energy left: {energy}" )
+        break
+    if energy >= int(distance):
+        energy -= int(distance)
+        won_battles += 1
+    else:
+        print(f"Not enough energy! Game ends with {won_battles} won battles and {energy} energy")
+        break
+    if won_battles % 3 == 0:
+        energy += won_battles
+
