db2e5f0431283b27b3840b098d26c7884c4c209d
zlatin-r <zlatinrusev@gmailcom>
2023-10-12 12:40:03 +0200

    Initial commit

--- Mid Exam Tasks/05. Programming Fundamentals Mid Exam/bonus_scoring_system.py
+++ Mid Exam Tasks/05. Programming Fundamentals Mid Exam/bonus_scoring_system.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from math import ceil
 
 number_of_students = int(input())
@@ -19,25 +18,3 @@ for student in range(number_of_students):
 
 print(f"Max Bonus: {ceil(max_points)}.")
 print(f"The student has attended {ceil(lectures)} lectures.")
-=======
-from math import ceil
-
-number_of_students = int(input())
-total_count_of_lectures = int(input())
-additional_bonuses = int(input())
-
-lectures = 0
-max_points = 0
-
-for student in range(number_of_students):
-    attendance = int(input())
-
-    total_bonus = (attendance / total_count_of_lectures) * (5 + additional_bonuses)
-
-    if total_bonus >= max_points:
-        max_points = total_bonus
-        lectures = attendance
-
-print(f"Max Bonus: {ceil(max_points)}.")
-print(f"The student has attended {ceil(lectures)} lectures.")
->>>>>>> origin/main
--- Mid Exam Tasks/05. Programming Fundamentals Mid Exam/inventory.py
+++ Mid Exam Tasks/05. Programming Fundamentals Mid Exam/inventory.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 items_journal = input().split(", ")
 
 while True:
@@ -29,41 +28,3 @@ while True:
             items_journal.remove(item)
             items_journal.append(item)
 
-
-
-
-=======
-items_journal = input().split(", ")
-
-while True:
-    command = input()
-    if command == "Craft!":
-        print(", ".join(items_journal))
-        break
-
-    command = command.split(" - ")
-    action = command[0]
-    item = command[1]
-
-    if action == "Collect":
-        if item not in items_journal:
-            items_journal.append(item)
-    elif action == "Drop":
-        if item in items_journal:
-            items_journal.remove(item)
-    elif action == "Combine Items":
-        item = item.split(":")
-        old_item = item[0]
-        new_item = item[1]
-        if old_item in items_journal:
-            index_old_item = items_journal.index(old_item)
-            items_journal.insert(index_old_item + 1, new_item)
-    elif action == "Renew":
-        if item in items_journal:
-            items_journal.remove(item)
-            items_journal.append(item)
-
-
-
-
->>>>>>> origin/main
