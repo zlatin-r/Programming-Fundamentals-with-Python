bcc0d8d35c8dce371f806cc9eaca6ff7c9da8ac7
zlatin-r <zlatinrusev@gmailcom>
2023-10-12 14:47:38 +0200

    Solution

--- Mid Exam Tasks/06. Programming Fundamentals Mid Exam Retake/man_o_war.py
+++ Mid Exam Tasks/06. Programming Fundamentals Mid Exam Retake/man_o_war.py
@@ -1,6 +1,7 @@
 pirate_ship = list(map(int, input().split(">")))
 war_ship = list(map(int, input().split(">")))
 max_health = int(input())
+stalemate = True
 
 while True:
     command = input().split()
@@ -12,10 +13,11 @@ while True:
         index = int(command[1])
         damage = int(command[2])
 
-        if 0 <= index <= len(war_ship) - 1:
+        if 0 <= index < len(war_ship):
             war_ship[index] -= damage
             if war_ship[index] <= 0:
                 print("You won! The enemy ship has sunken.")
+                stalemate = False
                 break
 
     elif action == "Defend":
@@ -23,7 +25,29 @@ while True:
         end_index = int(command[2])
         damage = int(command[3])
 
-        pirate_ship = [num - damage for num in pirate_ship if start_index <= pirate_ship.index(num) <= end_index]
-        xax
+        if 0 <= start_index < len(pirate_ship) and 0 <= end_index < len(pirate_ship):
+            for x in range(start_index, end_index + 1):
+                pirate_ship[x] -= damage
+                if pirate_ship[x] <= 0:
+                    print("You lost! The pirate ship has sunken.")
+                    stalemate = False
+                    break
 
-    print(pirate_ship)
+    elif action == "Repair":
+        index = int(command[1])
+        health = int(command[2])
+
+        if 0 <= index < len(pirate_ship):
+            pirate_ship[index] += health
+            if pirate_ship[index] < max_health:
+                pirate_ship[index] = max_health
+
+    elif action == "Status":
+        threshold = max_health * 0.20
+
+        sections_for_repair = [section for section in pirate_ship if section > threshold]
+        print(f"{len(sections_for_repair)} sections need repair.")
+
+if stalemate:
+    print(f"Pirate ship status: {sum(pirate_ship)}")
+    print(f"Warship status: {sum(war_ship)}")
