965062a0c180bd81da0c78c347b8cb4e9515bf65
zlatin-r <75085055+zlatin-r@users.noreply.github.com>
2023-11-02 16:43:08 +0100

    Solution

--- Dictionaries - Exercise/company_user_75points.py
+++ Dictionaries - Exercise/company_user_75points.py
@@ -2,7 +2,7 @@ data = {}
 command = input()
 
 while command != "End":
-    company_name, employee_id = command.split("->")
+    company_name, employee_id = command.split(" -> ")
 
     if company_name not in data:
         data[company_name] = []
@@ -13,8 +13,6 @@ while command != "End":
     command = input()
 
 for company, employees in data.items():
-    print(company.strip())
+    print(company)
     for ID in employees:
-        print(f"-- {ID.strip()}")
-
-
+        print(f"-- {ID}")
