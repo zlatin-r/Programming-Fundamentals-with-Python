87eb3ae0175d9ce06b30f297c2d82602f9f60ef4
zlatin-r <75085055+zlatin-r@users.noreply.github.com>
2023-11-06 14:57:06 +0100

    Solution

--- Dictionaries - Exercise/force_book.py
+++ Dictionaries - Exercise/force_book.py
@@ -20,9 +20,23 @@ while commands != "Lumpawaroo":
     elif "->" in commands:
         force_user, force_side = commands.split(" -> ")
 
+        for value in force_side_dictionary.values():
+            if force_user in value:
+                value.remove(force_user)
+                break
+        if force_side not in force_side_dictionary.keys():
+            force_side_dictionary[force_side] = []
+        force_side_dictionary[force_side].append(force_user)
+
         print(f"{force_user} joins the {force_side} side!")
 
     commands = input()
+
+for force_side, force_users in force_side_dictionary.items():
+    if len(force_users) > 0:
+        print(f"Side: {force_side}, Members: {len(force_users)}")
+        for force_user in force_users:
+            print(f"! {force_user}")
 
 
 
