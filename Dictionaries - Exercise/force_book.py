fe9316acf292b7bef472ee8b539845de17132885
zlatin-r <75085055+zlatin-r@users.noreply.github.com>
2023-11-06 14:47:34 +0100

    Solution

--- Dictionaries - Exercise/force_book.py
+++ Dictionaries - Exercise/force_book.py
@@ -1,15 +1,29 @@
+force_side_dictionary = {}
 commands = input()
-data = {}
 
-while True:
+
+while commands != "Lumpawaroo":
 
     if "|" in commands:
-        force, user = commands.split(" | ")
-    elif " -> " in commands:
-        user, force = commands.split(" -> ")
-    else:
-        break
+        force_side, force_user = commands.split(" | ")
+        user_found = False
 
-print(data)
+        for value in force_side_dictionary.values():
+            if force_user in value:
+                user_found = True
+                break
+        if not user_found:
+            if force_side not in force_side_dictionary.keys():
+                force_side_dictionary[force_side] = []
+            force_side_dictionary[force_side].append(force_user)
+
+    elif "->" in commands:
+        force_user, force_side = commands.split(" -> ")
+
+        print(f"{force_user} joins the {force_side} side!")
+
+    commands = input()
+
+
 
 
