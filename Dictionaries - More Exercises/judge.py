5709dce43561b372bee2cdfa2418bdc305aa4c8c
zlatin-r <75085055+zlatin-r@users.noreply.github.com>
2023-11-09 12:23:23 +0100

    Solution

--- Dictionaries - More Exercises/judge.py
+++ Dictionaries - More Exercises/judge.py
@@ -1,28 +1,24 @@
 data = {}
 total_points = {}
 data_input = input()
-
+count = 1
 
 while data_input != "no more time":
     user_name, contest, points = data_input.split(" -> ")
 
-    if contest not in data:
-        data[contest] = data.get(contest, {})
+    data[contest] = data.get(contest, {})
     data[contest][user_name] = data[contest].get(user_name, int(points))
     data[contest][user_name] = max(int(points), data[contest][user_name])
 
     data_input = input()
 
 for contest, user_name in data.items():
-    count = 1
     print(f"{contest}: {len(user_name)} participants")
-    for name, points in sorted(user_name.items(), key=lambda item: -item[1]):
-        if name not in total_points:
-            total_points[name] = 0
-        total_points[name] += int(points)
-        print(f"{count}. {name} <::> {points}")
-        count += 1
+    for pos, (name, points) in enumerate(sorted(user_name.items(), key=lambda item: -item[1]), 1):
+        print(f"{pos}. {name} <::> {points}")
+        total_points[name] = total_points.get(name, 0) + points
 
 print("Individual standings:")
-for name, points in sorted(total_points.items(), key=lambda item: -item[1]):
-    print(f"{name} -> {points}")
+for pos, (name, points) in enumerate(sorted(total_points.items(), key=lambda item: -item[1]), 1):
+    print(f"{pos}. {name} -> {points}")
+
