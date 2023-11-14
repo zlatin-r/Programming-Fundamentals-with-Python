150d58c553991041c3592637bab5e770fd722c41
zlatin-r <75085055+zlatin-r@users.noreply.github.com>
2023-11-14 10:46:01 +0100

    Solution

--- Text Processing - Exercise/letters_change_numbers.py
+++ Text Processing - Exercise/letters_change_numbers.py
@@ -1,20 +1,26 @@
 strings = input().split()
-number = ""
 final_sum = 0
 
-
 for string in strings:
+    number = ""
     first_letter = ord(string[0].lower()) - 96
     second_letter = ord(string[-1].lower()) - 96
 
+    for char in range(len(string)):
+        if string[char].isdigit():
+            number += string[char]
+    number = int(number)
+
     if string[0].isupper():
-        for char in range(len(string)):
-            if string[char].isdigit():
-                number += string[char]
-        number = int(number)
         number /= first_letter
-        number /= second_letter
+    else:
+        number *= first_letter
 
-    elif string[0].islower():
+    if string[-1].isupper():
+        number -= second_letter
+    else:
+        number += second_letter
 
+    final_sum += number
 
+print(f"{final_sum:.2f}")
