4815ac561201ad8236c629afcdeb5adcfc40f52f
zlatin-r <75085055+zlatin-r@users.noreply.github.com>
2023-11-17 14:56:43 +0100

    Solution

---
+++ Regular Expressions - Lab/match_numbers.py
@@ -0,0 +1,10 @@
+import re
+
+numbers = input()
+
+regex = r"(^|(?<=\s))-?([0]|[1-9][0-9]*)(\.\d+)?($|(?=\s))"
+
+matches = re.finditer(regex, numbers)
+
+for match in matches:
+    print(match.group(), end=" ")
