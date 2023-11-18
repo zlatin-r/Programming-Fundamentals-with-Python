6acf4f50b5e73726f4f50f4ba9f9c2111b841c22
zlatin-r <75085055+zlatin-r@users.noreply.github.com>
2023-11-18 15:53:34 +0100

    Solution to the task

---
+++ Regular Expressions - More Exercises/race.py
@@ -0,0 +1,22 @@
+import re
+
+names = input().split(", ")
+data = input()
+results = {}
+
+while data != "end of race":
+    match_name = re.findall(r"[A-Za-z]", data)
+    match_time = re.findall(r"\d", data)
+    name = "".join(match_name)
+    time = sum([int(x) for x in match_time])
+
+    if name in names:
+        if name not in results:
+            results[name] = 0
+        results[name] += time
+    data = input()
+
+sorted_results = sorted(results.items(), key=lambda item: item[1], reverse=True)
+print(f"1st place: {sorted_results[0][0]}")
+print(f"2nd place: {sorted_results[1][0]}")
+print(f"3rd place: {sorted_results[2][0]}")
