bbbb1c2b37060a83c08f5da75aff41faa98a8e3c
zlatin-r <75085055+zlatin-r@users.noreply.github.com>
2023-11-20 17:28:43 +0100

    Solution to the task

---
+++ Final Exams/world_tour.py
@@ -0,0 +1,33 @@
+destinations = input()
+
+
+def valid(index):
+    if 0 <= index < len(destinations):
+        return True
+
+
+command = input()
+while command != "Travel":
+    command = command.split(":")
+    action = command[0]
+
+    if action == "Add Stop":
+        index, stop = int(command[1]), command[2]
+        if valid(index):
+            destinations = destinations[:index] + stop + destinations[index:]
+            print(destinations)
+
+    elif action == "Remove Stop":
+        start_index, end_index = int(command[1]), int(command[2])
+        destinations = destinations[:start_index] + destinations[end_index + 1:]
+        print(destinations)
+
+    elif action == "Switch":
+        old_string, new_string = command[1], command[2]
+        if old_string in destinations:
+            destinations = destinations.replace(old_string, new_string)
+        print(destinations)
+
+    command = input()
+
+print(f"Ready for world tour! Planned stops: {destinations}")
