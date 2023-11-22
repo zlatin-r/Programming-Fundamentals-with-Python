416328b57e8f0603767e5ee1ebb004c9f213c0b6
zlatin-r <75085055+zlatin-r@users.noreply.github.com>
2023-11-22 18:19:36 +0100

    Solution to the task

--- Final Exams/need_for_speed_III.py
+++ Final Exams/need_for_speed_III.py
@@ -12,23 +12,28 @@ while command != "Stop":
 
     if action == "Drive":
         distance, needed_fuel = int(command[2]), int(command[3])
+
         if cars_data[car][1] < needed_fuel:
             print("Not enough fuel to make that ride")
         else:
             cars_data[car][0] += distance
             cars_data[car][1] -= needed_fuel
             print(f"{car} driven for {distance} kilometers. {needed_fuel} liters of fuel consumed.")
+
         if cars_data[car][0] >= 100000:
             print(f"Time to sell the {car}!")
             cars_data.pop(car)
+
     elif action == "Refuel":
         fuel_to_fill = int(command[2])
         fuel_in_tank = cars_data[car][1]
         cars_data[car][1] = min(cars_data[car][1] + fuel_to_fill, 75)
         print(f"{car} refueled with {cars_data[car][1] - fuel_in_tank} liters")
+
     elif action == "Revert":
         kilometers = int(command[2])
         cars_data[car][0] -= kilometers
+
         if cars_data[car][0] < 10000:
             cars_data[car][0] = 10000
         print(f"{car} mileage decreased by {kilometers} kilometers")
