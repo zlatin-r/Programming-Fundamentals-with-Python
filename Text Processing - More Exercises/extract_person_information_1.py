9ddde88fe3f1ba3ae7124d46ad7c3ac696fcd21c
zlatin-r <75085055+zlatin-r@users.noreply.github.com>
2023-11-29 10:24:14 +0100

    Descriptions

---
+++ Text Processing - More Exercises/extract_person_information_1.py
@@ -0,0 +1,16 @@
+num_names = int(input())
+name = ""
+age = ""
+
+for _ in range(num_names):
+    string = input()
+
+    index_1 = string.index("@")
+    index_2 = string.index("|")
+    index_3 = string.index("#")
+    index_4 = string.index("*")
+
+    name = string[index_1 + 1:index_2]
+    age = string[index_3 + 1:index_4]
+
+    print(f"{name} is {age} years old.")
