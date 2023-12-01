a583b1c900957f49635e2e6e63518c373283c6a9
zlatin-r <75085055+zlatin-r@users.noreply.github.com>
2023-12-01 16:22:35 +0100

    Solution to the task

--- Final Exams/plant_discovery.py
+++
@@ -1,7 +0,0 @@
-number_of_plants = int(input())
-
-plants_info = {}
-
-for plant in range(number_of_plants):
-    plant_name, rarity = input().split("<->")
-    plants_info[plant_name] = [rarity, []]
