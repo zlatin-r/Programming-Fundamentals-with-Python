414c07d5ef079d6500573cb152814a5c41b7be38
zlatin-r <75085055+zlatin-r@users.noreply.github.com>
2023-12-01 17:46:56 +0100

    Solution to the task

--- Final Exams/secret_chat_1.py
+++ Final Exams/secret_chat_1.py
@@ -2,19 +2,25 @@ message = input()
 
 
 def insert_space(text, index):
-    return text[:index] + " " + text[index:]
+    text = text[:index] + " " + text[index:]
+    print(text)
+    return text
 
 
 def reverse_string(text, string):
     if string in text:
         text = text.replace(string, "", 1) + string[::-1]
+        print(text)
         return text
     else:
         print("error")
+        return text
 
 
 def change_all(text, old_string, new_string):
-    return text.replace(old_string, new_string)
+    text = text.replace(old_string, new_string)
+    print(text)
+    return text
 
 
 command = input()
@@ -31,6 +37,5 @@ while command != "Reveal":
     elif action == "ChangeAll":
         message = change_all(message, command[1], command[2])
 
-    print(message)
     command = input()
 print(f"You have a new text message: {message}")
