d6ed32ba7a6636191abba6b67c3231a641f4cfbc
zlatin-r <75085055+zlatin-r@users.noreply.github.com>
2023-12-03 13:59:09 +0100

    Solution to the task

---
+++ Final Exam 03.02.2023/boss_rush.py
@@ -0,0 +1,19 @@
+import re
+
+n = int(input())
+pattern = r"([|])([A-Z]{4,})\1:([#])([A-Za-z\s]+)\3"
+
+
+for i in range(n):
+    data = input()
+    match = re.findall(pattern, data)
+
+    if match and match[0][3].count(" ") == 1:
+        name = match[0][1]
+        title = match[0][3]
+
+        print(f"{name}, The {title}")
+        print(f">> Strength: {len(name)}")
+        print(f">> Armor: {len(title)}")
+    else:
+        print("Access denied!")
